41c2f49c7c7e2871dd63bec7c229b2418f11cc9f
scott@refactr.com <scott@refactr.com@2f81676e-3cdf-11de-b5fc-91815ed80701>
2009-06-01 14:07:07 +0000

    move example links out of resources.groovy

--- trunk/grails-app/conf/spring/resources.groovy
+++ trunk/grails-app/conf/spring/resources.groovy
@@ -14,29 +14,3 @@ beans = {
 		} 
 	}
 }
-
-
-// examples/docs:
-// 
-// http://forum.springsource.org/showpost.php?p=213252&postcount=9
-// 		? http://static.springframework.org/spring/docs/2.5.x/reference/aop.html#aop-ataspectj-advice-params
-// 		? http://forum.springsource.org/showthread.php?t=30238
-// 		? http://www.experts-exchange.com/Programming/Languages/Java/Q_21269825.html
-// 		? http://jira.springframework.org/browse/SPR-3174
-// 		? http://static.springframework.org/spring/docs/2.0.x/reference/xsd-config.html
-// 		? http://www.springframework.org/schema/aop/spring-aop.xsd
-// 		? http://forum.springsource.org/showthread.php?p=209424
-// 		? http://www.javabeat.net/tips/85-how-to-write-interception-around-advice-in-sp.html
-// 		? http://www.javalobby.org/java/forums/t44746.html
-// 		? http://www.packtpub.com/article/design-with-spring-aop
-// 		? http://static.springframework.org/spring/docs/2.5.x/reference/index.html
-// 		? http://static.springframework.org/spring/docs/2.5.x/reference/aop.html#aop-schema
-// 		? http://static.springframework.org/spring/docs/2.5.x/reference/aop-api.html
-// 		? http://grails.org/doc/1.1/guide/14.%20Grails%20and%20Spring.html
-// 		? http://www.zabada.com/technology/aop-example.html
-// 		? http://www.infoq.com/articles/Orchestration-Oleg-Zhurakousky
-// 		? http://www.jroller.com/kenwdelong/entry/circuit_breaker_in_java
-// 		? http://static.springframework.org/spring/docs/2.0.x/api/org/springframework/aop/aspectj/AbstractAspectJAdvice.html
-// 		? http://forum.springsource.org/showthread.php?t=62460
-
-
